b11b4d3fa7265f1c5df30d489db2b3e4f135efc9
Am159632 <asaf.margaliot@gmail.com>
2026-04-05 21:11:21 +0300

    Time: 0 ms (100%), Space: 42 MB (64.63%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countOrders(int n) {
+        long res = 1;
+        long mod = 1_000_000_007;
+        
+        for (int i = 1; i <= n; i++) {
+            long currentMultiplier = i * (2 * i - 1);
+            
+            res = (res * currentMultiplier) % mod;
+        }
+        
+        return (int) res; 
+    }
+}
