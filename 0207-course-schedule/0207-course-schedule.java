460a2a07b2867f2e290bfb67b21715283a7aedd3
Margaliot Asaf <asaf.margaliot@gmail.com>
2026-05-26 14:32:46 +0300

    Time: 4 ms (93.31%), Space: 47.5 MB (7.78%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> list=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            List<Integer> l=new ArrayList<>();
+            list.add(l);
+        }
+        for(int i=0;i<prerequisites.length;i++){
+            list.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+        int[] visited=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            if(visited[i]==0){
+                if(dfs(i,list,visited)) return false;
+            } 
+        }
+        return true;
+    }
+
+    public boolean dfs(int x,List<List<Integer>> list, int[] visited){
+        visited[x]=1;
+        for(int i=0;i<list.get(x).size();i++){
+            if(visited[list.get(x).get(i)]==1) return true;
+            if(visited[list.get(x).get(i)]==0) {
+                 if(dfs(list.get(x).get(i),list,visited)) return true;
+            }
+        }
+        visited[x]=2;
+        return false;
+    }
+}
