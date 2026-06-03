7058ac6ba384a9935ddbd745bf9959801a3a2ae6
Margaliot Asaf <asaf.margaliot@gmail.com>
2026-06-03 11:24:10 +0300

    Time: 4 ms (92.04%), Space: 47.9 MB (33%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int row=img.length;
+        int col=img[0].length;
+        int[][] res=new int [row][col];
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                int rs=Math.max(i-1,0);
+                int re=Math.min(i+1,row-1);
+                int cs=Math.max(j-1,0);
+                int ce=Math.min(j+1,col-1);
+                int sum=0;
+                for(int k=rs;k<=re;k++){
+                    for(int t=cs;t<=ce;t++){
+                        sum+=img[k][t];
+                    }
+                }
+                res[i][j]=sum/((re-rs+1)*(ce-cs+1));
+            }
+        }
+        return res;
+    }
+}
