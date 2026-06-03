bf7aaf5202c00be55ccf74ae3f8b730d5fbde12e
Margaliot Asaf <asaf.margaliot@gmail.com>
2026-06-03 11:32:00 +0300

    Time: 6 ms (68.6%), Space: 47.8 MB (50.51%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.java
+++ 0661-image-smoother/0661-image-smoother.java
@@ -6,17 +6,15 @@ class Solution {
 
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                int rs=Math.max(i-1,0);
-                int re=Math.min(i+1,row-1);
-                int cs=Math.max(j-1,0);
-                int ce=Math.min(j+1,col-1);
                 int sum=0;
-                for(int k=rs;k<=re;k++){
-                    for(int t=cs;t<=ce;t++){
+                int count=0;
+                for(int k=Math.max(i-1,0);k<=Math.min(i+1,row-1);k++){
+                    for(int t=Math.max(j-1,0);t<=Math.min(j+1,col-1);t++){
                         sum+=img[k][t];
+                        count++;
                     }
                 }
-                res[i][j]=sum/((re-rs+1)*(ce-cs+1));
+                res[i][j]=sum/count;
             }
         }
         return res;
