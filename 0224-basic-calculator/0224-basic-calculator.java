53ac4235ba29aebc7e68b3eac1d49f39dc6c50dc
Margaliot Asaf <asaf.margaliot@gmail.com>
2026-06-11 10:24:35 +0300

    Time: 3 ms (99.85%), Space: 46.3 MB (84.67%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -3,24 +3,29 @@ class Solution {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        
         char[] chars = s.toCharArray();
-        int n = chars.length;
-        
-        int[] stack = new int[n];
-        int top = 0;
-        
+        int[] index = new int[1];
+        return helper(chars, index);
+    }
+
+    private int helper(char[] chars, int[] index) {
         int result = 0;
         int sign = 1;
-        
-        for (int i = 0; i < n; i++) {
-            char c = chars[i];
-            
+        int n = chars.length;
+
+        while (index[0] < n) {
+            char c = chars[index[0]];
+
+            if (c == ' ') {
+                index[0]++;
+                continue;
+            }
+
             if (c >= '0' && c <= '9') {
                 int num = c - '0';
-                while (i + 1 < n && chars[i + 1] >= '0' && chars[i + 1] <= '9') {
-                    num = num * 10 + (chars[i + 1] - '0');
-                    i++;
+                while (index[0] + 1 < n && chars[index[0] + 1] >= '0' && chars[index[0] + 1] <= '9') {
+                    num = num * 10 + (chars[index[0] + 1] - '0');
+                    index[0]++;
                 }
                 result += num * sign;
             } else if (c == '+') {
@@ -28,16 +33,14 @@ class Solution {
             } else if (c == '-') {
                 sign = -1;
             } else if (c == '(') {
-                stack[top++] = result;
-                stack[top++] = sign;
-                result = 0;
-                sign = 1;
+                index[0]++;
+                result += sign * helper(chars, index);
             } else if (c == ')') {
-                result *= stack[--top];
-                result += stack[--top];
+                return result;
             }
+            index[0]++;
         }
-        
+
         return result;
     }
 }
